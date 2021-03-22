d3c0a65589660f0b97cdaea31887bcda36f2072a
Zubenna <emelunna@gmail.com>
2021-03-22 12:36:40 +0100

    Adding Slide Functionality to The Testimony Section

--- main.js
+++ main.js
@@ -85,30 +85,31 @@ for (i = 0; i < coll.length; i++) {
 
 
 // Testimony Slide
+const $prev = document.querySelector('.prev');
+const $next = document.querySelector('.next');
 
-var slideIndex = 1;
+$prev.addEventListener('click', () => {
+  plusSlides(-1);
+})
+
+$next.addEventListener('click', () => {
+  plusSlides(1);
+})
+
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
 }
