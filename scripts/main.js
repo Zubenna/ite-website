55f3e833278a4a972b603a2084ad2c9e45b8161a
Zubenna <emelunna@gmail.com>
2021-04-02 07:03:02 +0100

    Correcting stylint errors

--- scripts/main.js
+++ scripts/main.js
@@ -113,3 +113,43 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";  
 }
+
+// Scrolling Elements
+const scrollElements = document.querySelectorAll(".el-scroll");
+
+const elementInView = (el, dividend = 1) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) / dividend
+  );
+};
+
+const elementOutofView = (el) => {
+  const elementTop = el.getBoundingClientRect().top;
+  return (
+    elementTop > (window.innerHeight || document.documentElement.clientHeight)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 1.25)) {
+      displayScrollElement(el);
+    } else if (elementOutofView(el)) {
+      hideScrollElement(el)
+    }
+  })
+}
+
+window.addEventListener("scroll", () => { 
+  handleScrollAnimation();
+});
