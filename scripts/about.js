cc4cd9b30658a0f62f77d869ef1002d6d9e7a384
Zubenna <emelunna@gmail.com>
2021-04-27 16:56:08 +0100

    Seperated About page into 4 different pages

--- scripts/about.js
+++ scripts/about.js
@@ -45,14 +45,6 @@ function displayAboutMenu(evt, testId) {
     }  
 }
 
-// function hideAboutMenu(testId) {
-//   if (aboutDiv.classList.contains("show-sub-menu") === true) {
-//     aboutDiv.classList.remove("show-sub-menu");
-//   }
-// }
-
-
-
 // Toggle menu button on mobile
 const selectElement = (element) => {
   return document.querySelector(element);
@@ -65,63 +57,6 @@ menuToggler.addEventListener("click", () => {
   body.classList.toggle("open");
 });
 
-// Implement Tab display
-let $tab1 = document.querySelector("#tab1");
-let $tab2 = document.querySelector("#tab2");
-let $tab3 = document.querySelector("#tab3");
-let $tab4 = document.querySelector("#tab4");
-
-$tab1.addEventListener("click", (event) => {
-  openCity(event, "tab-1-head");
-});
-
-$tab2.addEventListener("click", (event) => {
-  openCity(event, "tab-2-head");
-});
-
-$tab3.addEventListener("click", (event) => {
-  openCity(event, "tab-3-head");
-});
-
-$tab4.addEventListener("click", (event) => {
-  openCity(event, "tab-4-head");
-});
-
-function openCity(evt, tabId) {
-  let i, tabcontent, tablinks;
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(tabId).style.display = "block";
-  evt.currentTarget.className += " active";
-}
-
-// Implement Counter
-// const counters = document.querySelectorAll(".counter");
-// const speed = 800;
-
-// counters.forEach((count) => {
-//   const updateCounter = () => {
-//     const ourTarget = +count.getAttribute("data-target");
-//     const ourCounter = +count.innerText;
-//     const increment = ourTarget / speed;
-
-//     if (ourCounter < ourTarget) {
-//       count.innerText = (ourCounter + increment).toFixed(2);
-//       setTimeout(updateCounter, 1);
-//     } else {
-//       count.innerText = ourTarget;
-//     }
-//   };
-//   updateCounter();
-// });
-
-// Collapsible button
 let coll = document.getElementsByClassName("collapsible");
 let i;
 for (i = 0; i < coll.length; i++) {
@@ -138,40 +73,6 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
-
-// Testimony Slide
-// const $prev = document.querySelector(".prev");
-// const $next = document.querySelector(".next");
-
-// $prev.addEventListener("click", () => {
-//   plusSlides(-1);
-// });
-
-// $next.addEventListener("click", () => {
-//   plusSlides(1);
-// });
-
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// function plusSlides(n) {
-//   showSlides((slideIndex += n));
-// }
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   if (n > slides.length) {
-//     slideIndex = 1;
-//   }
-//   if (n < 1) {
-//     slideIndex = slides.length;
-//   }
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   slides[slideIndex - 1].style.display = "block";
-// }
 
 // Scrolling Elements
 const scrollElements = document.querySelectorAll(".el-scroll");
